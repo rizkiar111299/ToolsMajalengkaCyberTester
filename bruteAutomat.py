42e959e2c914a8a266bacb6aad76867037c6d323
Rizki Alam Ramdhani <45142541+rizkiar111299@users.noreply.github.com>
2022-09-15 10:53:13 +0700

    Delete bruteAutomat.py

--- bruteAutomat.py
+++
@@ -1,6 +0,0 @@
-from webbot import Browser
-web = Browser("C:\Program Files\Google\Chrome\Application\chrome.exe")
-web.go_to('https://satu.unma.ac.id/')
-web.type('password',username = '18.14.1.0004',classname="form-input")
-web.type('username',username = '@Mrky404',classname="form-input")
-web.click('Masuk')
